934318058a3d5e3b742279ed54bb6fc13aab0b7e
LokkiSignalsBot <lokki6992@gmail.com>
2025-07-30 01:24:08 +0300

    Update Update bot.py

--- Update bot.py
+++ Update bot.py
@@ -1,19 +1,1 @@
-import os
-import telebot
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет, я готов!")
-
-def send_signal(text):
-    bot.send_message(CHAT_ID, text)
-
-if __name__ == '__main__':
-    bot.polling()
