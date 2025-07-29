dc5f1d7b3e3ab32bfbfe93147b5478705a8e6c97
LokkiSignalsBot <lokki6992@gmail.com>
2025-07-30 01:37:43 +0300

    Update LokkiBot.py

--- LokkiBot.py
+++ LokkiBot.py
@@ -1,11 +1,15 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+BOT_TOKEN = "8021467739:AAHqkVoZnCeJbifEI-DchPzoMKks98zfGA"
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я Lokki Signals Bot.")
+    await update.message.reply_text("Привет! Бот работает. ✅")
 
-app = Application.builder().token("8021467739:AAHqkVoZnCeJbifEI-DchPzoMKks98zfGA").build()
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+if __name__ == "__main__":
+    main()
