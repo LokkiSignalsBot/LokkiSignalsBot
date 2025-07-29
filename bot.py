1f4143aa4d2c1979cb355585c501e0e5ae2972a5
LokkiSignalsBot <lokki6992@gmail.com>
2025-07-29 23:33:46 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет! Бот запущен и готов присылать сигналы.")
+
+def send_signal(text):
+    bot.send_message(CHAT_ID, text)
+
+if __name__ == '__main__':
+    bot.polling()
