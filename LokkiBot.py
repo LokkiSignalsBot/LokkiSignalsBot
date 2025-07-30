79616703402387ad04bf391ee6a009363611c15e
LokkiSignalsBot <lokki6992@gmail.com>
2025-07-31 00:56:12 +0300

    Update LokkiBot.py

--- LokkiBot.py
+++ LokkiBot.py
@@ -1,23 +1,20 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from dotenv import load_dotenv
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Укажи в Render переменную с доменом
+TOKEN = os.environ.get("BOT_TOKEN")
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот на Webhook работает ✅")
+    await update.message.reply_text("Бот запущен через Webhook и работает!")
 
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
 
-    app.run_webhook(
+    application.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 8443)),
+        port=int(os.environ.get("PORT", 10000)),
         webhook_url=WEBHOOK_URL
     )
 
