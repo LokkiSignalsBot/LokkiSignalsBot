7e93b519b84970991c53ab65ec704c206dd768b5
LokkiSignalsBot <lokki6992@gmail.com>
2025-07-31 01:02:01 +0300

    Update LokkiBot.py

--- LokkiBot.py
+++ LokkiBot.py
@@ -2,17 +2,17 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот запущен через Webhook и работает!")
+    await update.message.reply_text("Бот успешно запущен по Webhook!")
 
 def main():
-    application = Application.builder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-    application.run_webhook(
+    app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 10000)),
         webhook_url=WEBHOOK_URL
